7323552f1d5132ccdc7ef0660cc84cf1d94bd5cf
MiguelRS-19 <reyessanchezmiguel2009@gmail.com>
2023-01-18 22:45:23 -0500

    update for production

--- main/app.js
+++ main/app.js
@@ -12,28 +12,28 @@ closeMenuBtn.addEventListener("click", toggleMenu);
 const menuLinks = document.querySelectorAll('.menu a[href^="#"]');
 
 const observer = new IntersectionObserver(
-  (entries) => {
-    entries.forEach((entry) => {
-      const id = entry.target.getAttribute("id");
-      const menuLink = document.querySelector(`.menu a[href="#${id}"]`);
+    (entries) => {
+        entries.forEach((entry) => {
+            const id = entry.target.getAttribute("id");
+            const menuLink = document.querySelector(`.menu a[href="#${id}"]`);
 
-      if (entry.isIntersecting) {
-        document.querySelector(".menu a.selected").classList.remove("selected");
-        menuLink.classList.add("selected");
-      }
-    });
-  },
-  { rootMargin: "-30% 0px -70% 0px" }
+            if (entry.isIntersecting) {
+                document.querySelector(".menu a.selected").classList.remove("selected");
+                menuLink.classList.add("selected");
+            }
+        });
+    },
+    { rootMargin: "-30% 0px -70% 0px" }
 );
 
 menuLinks.forEach((menuLink) => {
-  menuLink.addEventListener("click", function () {
-    menu.classList.remove("menu_opened");
-  });
+    menuLink.addEventListener("click", function () {
+        menu.classList.remove("menu_opened");
+    });
 
-  const hash = menuLink.getAttribute("href");
-  const target = document.querySelector(hash);
-  if (target) {
-    observer.observe(target);
-  }
+    const hash = menuLink.getAttribute("href");
+    const target = document.querySelector(hash);
+    if (target) {
+        observer.observe(target);
+    }
 });
